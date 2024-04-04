3be5bca1e76bdcce0c3c4edb7b7b99f47104363a
BishalDey908 <ronydey34567@gmail.com>
2024-04-04 10:14:13 +0530

    ....

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ let data;
 //--------------Middleware-------------------
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://mern-ecommerce-eta-five.vercel.app/"],
     methods: ["GET", "POST"],
     credentials: true,
   })
